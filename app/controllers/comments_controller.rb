b79daa9b06b263df74d31877440c34911a0e8228
Thomas <muaddatr@protonmail.com>
2022-04-13 23:38:32 +0200

    adding authentication to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user!, only: [:create]
+
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         redirect_to post_path(@comment.post)
     end
-    
+
     def comment_params
         params.require(:comment).permit(:author, :body)
     end
